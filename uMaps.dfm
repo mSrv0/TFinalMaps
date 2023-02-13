cdf7aa4c0ab7740ac430565204d5dcfcda063e12
mSrv0 <mreus@puenteweb.com>
2023-02-13 10:53:21 -0300

    Initial commit

---
+++ uMaps.dfm
@@ -0,0 +1,27 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 162
+  ClientWidth = 248
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  WindowState = wsMaximized
+  OnActivate = FormActivate
+  OnCreate = FormCreate
+  TextHeight = 15
+  object SpeedButton1: TSpeedButton
+    Left = 0
+    Top = 140
+    Width = 248
+    Height = 22
+    Align = alBottom
+    ExplicitLeft = 120
+    ExplicitTop = 88
+    ExplicitWidth = 23
+  end
+end
---
+++ uMaps.pas
@@ -0,0 +1,200 @@
+unit uMaps;
+
+interface
+
+uses
+    Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, VCL.TMSFNCMapsCommonTypes,
+  VCL.TMSFNCTypes, VCL.TMSFNCUtils, VCL.TMSFNCGraphics, VCL.TMSFNCGraphicsTypes,
+  VCL.TMSFNCGeocoding, VCL.TMSFNCCustomComponent, VCL.TMSFNCCloudBase,
+  VCL.TMSFNCStaticMap, VCL.TMSFNCCustomControl, VCL.TMSFNCWebBrowser,
+  VCL.TMSFNCMaps, VCL.TMSFNCMapsImage, Vcl.Buttons, Vcl.ExtCtrls,
+  VCL.TMSFNCRouteCalculator, Vcl.StdCtrls ;
+
+type
+  TForm1 = class(TForm)
+    SpeedButton1: TSpeedButton;
+    procedure FormActivate(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    {procedure SpeedButton1Click(Sender: TObject);
+    procedure TM_ActualizarTimer(Sender: TObject);}
+  private
+    rute : TTMSFNCRouteCalculatorRoute;
+    FFNCGeocoding: TTMSFNCGeocoding;
+    FFNCMaps: TTMSFNCMaps;
+    FFNCRouteCalculator : TTMSFNCRouteCalculator;
+    FInicie: Boolean;
+    procedure CrearMapa;
+    procedure generarRutas(Sender: TObject; const ARequest: TTMSFNCGeocodingRequest; const ARequestResult: TTMSFNCCloudBaseRequestResult);
+
+   // procedure routeGeocoding(Sender: TObject; const ARequest: TTMSFNCGeocodingRequest; const ARequestResult: TTMSFNCCloudBaseRequestResult);
+
+    procedure FFNCRouteCalculatorGetGeocoding(Sender: TObject;
+      const ARequest: TTMSFNCGeocodingRequest;
+      const ARequestResult: TTMSFNCCloudBaseRequestResult);
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.CrearMapa;
+var x: TTMSFNCGeocodingRequest;
+    y: TTMSFNCCloudBaseRequestResult;
+    item: TTMSFNCGeocodingRequest;
+    itemgO, itemgD, itemgN: TTMSFNCGeocodingItem;
+begin
+  FInicie:= True;
+
+  FFNCRouteCalculator := TTMSFNCRouteCalculator.Create(self);
+  //FFNCRouteCalculator.OnGetGeocoding:= generarRutas;
+  //FNCRouteCalculator.OnGetRouteGeocoding:= routeGeocoding;
+
+  FFNCMaps:= TTMSFNCMaps.Create(nil);
+  FFNCGeocoding:= TTMSFNCGeocoding.Create(Self);
+  FFNCMaps.APIKey:= 'AIzaSyBPlZwdSxT0vu7yDhm351Gu3_L3NDdNPO8';
+  FFNCGeocoding.APIKey := 'AIzaSyBPlZwdSxT0vu7yDhm351Gu3_L3NDdNPO8';
+  FFNCRouteCalculator.APIKey := 'AIzaSyBPlZwdSxT0vu7yDhm351Gu3_L3NDdNPO8';
+
+  FFNCMaps.Parent:= Self;
+  FFNCMaps.Align:= TAlign.alClient;
+  FFNCMaps.Service := msGoogleMaps;
+  FFNCMaps.RouteCalculator := FFNCRouteCalculator;
+
+  //FFNCGeocoding.OnGetGeocoding:= generarRutas;
+  FFNCGeocoding.Service := gsGoogle;
+  item:= FFNCGeocoding.GeocodingRequests.Add;
+  itemgO:= item.Items.Add;
+  itemgO.Address:= 'Presidente Arturo H. Illia 517 Neuquen';
+  itemgD:= item.Items.Add;
+  itemgD.Address:= 'Entre Rios 1015 Neuquen';
+  itemgN := item.Items.Add;
+  itemgN.Address := 'Velez Sarsfield 863 Neuquen';
+  //FFNCGeocoding.GetGeocoding(itemgO.Address, nil, 'origin');
+  //FFNCGeocoding.GetGeocoding(itemgD.Address, nil, 'destination');
+  //FFNCRouteCalculator.GetGeocoding(itemgO.Address, nil, 'origin');
+  //FFNCRouteCalculator.GetGeocoding(itemgD.Address, nil, 'destination');
+
+  //FFNCRouteCalculatorGetGeocoding(FFNCGeocoding, item, TTMSFNCCloudBaseRequestResult);
+
+  FFNCRouteCalculator.Active := True;
+  FFNCRouteCalculator.Options.HistoryEnabled := True;
+  FFNCRouteCalculator.Service := csGoogle;
+  //FFNCRouteCalculator.OnGetRouteGeocoding(item,);
+  {//  x:= FFNCRouteCalculator.Routes.Add;
+  FFNCRouteCalculator.OnGetGeocoding := FFNCRouteCalculatorGetGeocoding;
+  FFNCRouteCalculator.GetGeocoding('Presidente Arturo H. Illia 517 Neuquen', nil, 'origin');
+  FFNCRouteCalculator.GetGeocoding('Entre Rios 1015 Neuquen', nil, 'destination');}
+  {x:= TTMSFNCGeocodingRequest.Create(item.Items);
+  y:= TTMSFNCCloudBaseRequestResult.Create;
+  FFNCRouteCalculatorGetGeocoding(FFNCGeocoding, x, y);}
+  //routeGeocoding(FFNCRouteCalculator, item, nil);
+  rute := TTMSFNCRouteCalculatorRoute.Create(nil);
+  rute.RouteName := 'Recorrido PW';
+  //FFNCRouteCalculator.AddRouteSegment(rute ,itemgN.Address);
+  FFNCRouteCalculator.CalculateRoute(itemgO.Address, itemgD.Address, nil);
+  //FFNCRouteCalculator.OnCalculateRoute := rute;
+  rute.FirstSegment.Create(itemgO.Create();
+  FFNCRouteCalculator.first;
+  FFNCRouteCalculator.AddRouteSegment(rute, itemgN.Address);
+  Refresh
+end;
+
+procedure TForm1.FormActivate(Sender: TObject);
+begin
+  if (not FInicie) then
+    CrearMapa
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  FInicie:= False;
+end;
+
+procedure TForm1.generarRutas(Sender: TObject; const ARequest: TTMSFNCGeocodingRequest; const ARequestResult: TTMSFNCCloudBaseRequestResult);
+var
+ I: Integer;
+ it: TTMSFNCGeocodingItem;
+ FStartAddress, FEndAddress: TTMSFNCMapsCoordinateRec;
+begin
+  for I := 0 to ARequest.Items.Count - 1 do
+    begin
+    it := ARequest.Items[I];
+    if ARequest.ID = 'origin' then
+    begin
+    FStartAddress := it.Coordinate.ToRec;
+    FFNCMaps.SetCenterCoordinate(FStartAddress);
+    FFNCMaps.AddMarker(FStartAddress);
+    end;
+    if ARequest.ID = 'destination' then
+      begin
+      FEndAddress := it.Coordinate.ToRec;
+      FFNCMaps.AddMarker(FEndAddress);
+      end;
+    end;
+end;
+
+{procedure TForm1.routeGeocoding(Sender: TObject; const ARequest: TTMSFNCGeocodingRequest; const ARequestResult: TTMSFNCCloudBaseRequestResult);
+var ACallback: TTMSFNCRouteCalculatorCalculateRouteCallback;
+    origen, destino : string;
+begin
+  origen:= ARequest.Items[0].Address;//'Presidente Arturo H. Illia 517 Neuquen';//;
+  destino:= ARequest.Items[1].Address; //'Entre Rios 1015 Neuquen';//;
+  FFNCRouteCalculator.CalculateRoute(origen, destino, ACallback);
+end;  }
+
+procedure TForm1.FFNCRouteCalculatorGetGeocoding(Sender: TObject; const ARequest: TTMSFNCGeocodingRequest; const ARequestResult: TTMSFNCCloudBaseRequestResult);
+begin
+  if ARequestResult.Success then
+    FFNCMaps.SetCenterCoordinate(ARequest.Items[0].Coordinate.ToRec);
+end;
+
+{
+procedure TForm1.CrearMapa;
+var
+ APIKey: string;
+begin
+ APIKey := 'AIzaSyBPlZwdSxT0vu7yDhm351Gu3_L3NDdNPO8';
+ FFNCRouteCalculator.Active := True;
+ FFNCRouteCalculator.Options.HistoryEnabled := True;
+ FFNCRouteCalculator.Service := csGoogle;
+ FFNCRouteCalculator.APIKey := APIKey;
+ FFNCMaps.RouteCalculator := FFNCRouteCalculator;
+ FFNCMaps.Service := msGoogleMaps;
+ FFNCMaps.APIKey := APIKey;
+end;
+}
+
+{
+procedure TForm1.generarRutas(Sender: TObject;
+  const ARequest: TTMSFNCGeocodingRequest;
+  const ARequestResult: TTMSFNCCloudBaseRequestResult);
+var
+ I: Integer;
+ it: TTMSFNCGeocodingItem;
+ FStartAddress, FEndAddress: TTMSFNCMapsCoordinateRec;
+begin
+  for I := 0 to ARequest.Items.Count - 1 do
+    begin
+    it := ARequest.Items[I];
+    if ARequest.ID = 'origin' then
+    begin
+    FStartAddress := it.Coordinate.ToRec;
+    FFNCMaps.SetCenterCoordinate(FStartAddress);
+    FFNCMaps.AddMarker(FStartAddress);
+    end;
+    if ARequest.ID = 'destination' then
+      begin
+      FEndAddress := it.Coordinate.ToRec;
+      FFNCMaps.AddMarker(FEndAddress);
+      end;
+    end;
+end;
+ }
+end.
